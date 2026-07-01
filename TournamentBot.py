2bb02c3659e95ed02f4fd62bcd66d86005a6ed71
AndiFriend <a.freund2010@web.de>
2026-07-01 21:14:44 +0200

    Added some changes

--- TournamentBot.py
+++ TournamentBot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import psycopg
 from discord.ext import commands
 from discord import app_commands
 from dotenv import load_dotenv
@@ -8,6 +9,10 @@ from keep_alive import keep_alive
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+
+conn = psycopg.connect(
+    "postgresql://neondb_owner:npg_rSwaRGpoA3j9@ep-green-darkness-aszevld8-pooler.c-4.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+)
 
 keep_alive()
 
@@ -31,4 +36,30 @@ async def greet(interaction: discord.Interaction):
     username = interaction.user.mention
     await interaction.response.send_message(f"Hello there, {username}")
 
-bot.run(TOKEN)
+@bot.tree.command(name="sign in", description="Signs you into the tournament as a player (not watcher)")
+async def signIn(
+    interaction: discord.Interaction, 
+    steam_username: str, 
+    be_substitute : bool
+):
+    discordID = interaction.user.id
+
+    try: 
+        cur = conn.cursor()
+        cur.execute(
+            """
+            INSERT INTO players (DiscordID, UserNameSteam, IsSubstitute)
+            VALUES (%s, %s, %s)
+            """,
+            (discordID, steam_username, be_substitute)
+        )
+        conn.commit()
+        
+    except psycopg.Error as e:
+        conn.rollback()
+        await interaction.response.send_message("The input data was invalid. Make sure that the Steam name is valid.", ephemeral=True)
+        return
+    finally:
+        cur.close()
+
+    await interaction.response.send_message("You are now signed in.", ephemeral=True)
